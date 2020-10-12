c7b25e45c1e3580803876422a27bf39cf4f03c0f
joel郑 <626279057@qq.com>
2020-10-12 21:40:06 +0800

    调整每次进入小程序都检查登录状态

--- App.vue
+++ App.vue
@@ -30,6 +30,9 @@
 			plus.screen.lockOrientation('portrait-primary');
 			// #endif
 			
+		},
+		onShow: function() {
+			console.log('App Show')
 			// 检查用户登录情况
 			// #ifdef H5
 			this.checkLogin();
@@ -37,11 +40,6 @@
 			// #ifdef MP-WEIXIN
 			this.$wechatMiniLogin();
 			// #endif
-		
-		},
-		onShow: function() {
-			console.log('App Show')
-			
 		},
 		onHide: function() {
 			console.log('App Hide')
